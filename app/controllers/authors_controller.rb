d89aa759ca272f35d34c7e717badfc0676903bec
Brian Nicholls <nichol88@gmail.com>
2020-02-28 15:54:46 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,19 +1,42 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+
+  def index
+    @authors = Author.all
+  end
+
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
     @author = Author.new
   end
 
+  def edit
+  end
+
+  def update
+    if @author.update author_params
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_author
+    @author = Author.find params[:id]
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,38 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+  def create
+    @post = Post.create post_params
+    if @post.valid?
+      redirect_to post_path @post
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_post
+    @post = Post.find params[:id]
+  end
 
   def post_params
     params.permit(:title, :category, :content)
